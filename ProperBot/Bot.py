2aedace49ae0f1b0168e19dd62ffff9233a584b5
Kxbir31 <kabirbhardwaj026@gmail.com>
2026-07-25 23:10:05 +0530

    .

---
+++ ProperBot/Bot.py
@@ -0,0 +1,28 @@
+from tempfile import template
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+import streamlit as st
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-V4-Pro",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_ACCESS_TOKEN"),
+    temperature=0.8
+)
+model = ChatHuggingFace(llm = llm)
+chat_history = []
+while True:
+    user = input("You :")
+    chat_history.append(user)
+    if user == "exit":
+        break
+    result = model.invoke(chat_history)
+    chat_history.append(result.content)
+    print("Ai : ",result.content)
+
+print(chat_history)
